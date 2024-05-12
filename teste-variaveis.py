e0e2355df831883f6cf59c61e81dee8f55af82db
nat-1502 <natalisoares90@outlook.com>
2024-05-12 08:19:29 -0300

    teste variaveis

---
+++ teste-variaveis.py
@@ -0,0 +1,15 @@
+#teste
+
+nome = 'Natali'    
+sobrenome = 'Soares da Silva'
+idade = 25
+ano_nascimento = 2024 - idade
+maior_idade = idade >= 18
+altura = 1.65
+
+print('Nome:', nome)
+print('Sobrenome', sobrenome)
+print('Idade', idade)
+print('Ano de nascimento', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em metros:', altura)
