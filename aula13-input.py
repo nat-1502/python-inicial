300b5e4e2ae26da1fbcdc7869c99da6d867b6f9e
nat-1502 <natalisoares90@outlook.com>
2024-05-15 10:48:43 -0300

    usando os condicionais

--- aula13-input.py
+++ aula13-input.py
@@ -8,3 +8,6 @@ int_numero_1 = int(numero_1)
 int_numero_2 = int(numero_2)
 
 print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
+
+# nome = input('Qual o seu nome? ')
+# print(f'O seu nome é {nome}')
