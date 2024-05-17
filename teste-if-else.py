9574b5b9c302554aa91525651a44a5fcfe7b6966
nat-1502 <natalisoares90@outlook.com>
2024-05-17 06:46:49 -0300

    operador and

---
+++ teste-if-else.py
@@ -0,0 +1,9 @@
+
+primeiro_valor = input ('Digite um valor:')
+segundo_valor = input ('Digite outro valor:')
+
+
+if primeiro_valor >= segundo_valor:
+    print (f'O Primeiro valor é maior ou igual ao segundo')
+    
+else: print (f'O Segundo valor é maior que o primeiro')
