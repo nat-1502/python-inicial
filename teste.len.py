242177f022d5f3ea1167a4c7996e16a8b400ac79
nat-1502 <natalisoares90@outlook.com>
2024-05-19 08:38:22 -0300

    teste

---
+++ teste.len.py
@@ -0,0 +1,34 @@
+
+""""
+Exercício
+Peça ao usuário para digitar seu nome
+Peça ao usuário para digitar sua idade
+Se nome e idade forem digitados:
+    Exiba:
+        Seu nome é {nome}
+        Seu nome invertido é {nome invertido}
+        Seu nome contém (ou não) espaços
+        Seu nome tem {n} letras
+        A primeira letra do seu nome é {letra}
+        A última letra do seu nome é {letra}
+Se nada for digitado em nome ou idade: 
+    exiba "Desculpe, você deixou campos vazios."
+"""
+
+nome = input('Digite seu nome por favor ' )
+idade = input('Agora, digite sua idade ' ) 
+
+if nome and idade:
+    print("Seu nome é:", nome)
+    print("Seu nome invertido é: ", nome[::-1])
+    if '' in nome: 
+        print("Seu nome contem espaços")
+    else: 
+        print("Seu nome não contem espaços")
+        
+    print("Seu nome tem: ", len (nome), "letras")
+    print("A primeira letra do seu nome é: ", nome[0])
+    print("A ultima letra do seu nome é: ", nome[-1])
+
+else:
+    print("Desculpe, você deixou campos vazios.")
